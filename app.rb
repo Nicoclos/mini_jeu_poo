b03aa5d7a4b16cb05a52bad1bc2a5e9b89976547
nicoclos <n.bia@outlook.fr>
2021-10-19 19:36:32 +0200

    initialisation.3

--- app.rb
+++ app.rb
@@ -4,4 +4,28 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+
+def player_uno
+    player1 = Player.new("Josiane")
+end
+
+def player_dos
+    player2 = Player.new("José")    
+end
+
+def healthy
+    puts "Voici l'état de forme de nos combattants du jour :"
+    puts player_uno.show_state
+    puts player_dos.show_state
+end
+
+def announce
+    puts "QUE LA BASTON COMMENCE ! ON VEUT DU SANG PUTAIN !"
+end
+
+def fight
+    player_uno.attacks(player_dos)
+    player_dos.attacks(player_uno)
+end
 binding.pry
+
---
+++ lib/player.rb
@@ -0,0 +1,40 @@
+class Player
+    attr_accessor :name, :life_points
+
+    def initialize(player_name)
+        @name = player_name
+        @life_points = 10
+    end
+
+    def show_state
+        puts "#{name} a #{life_points} points de vie !"
+    end
+
+    def gets_damage(damage)
+        @life_points - damage
+        if @life_points <= 0
+            puts "#{name} a perdu tous ses PV ! #{name} lâche 20 bitcoins en s'effondrant et se fait absorber par le sol pour rejoindre les entrailles de la Terre..."
+        end
+    end
+
+    def attacks(foe)
+        puts "#{name} s'en prend violemment à #{foe.name} ! Ça va barder !"
+        damage_dealt = compute_damage
+        puts "#{name} inflige #{damage_dealt} points de dégâts !"
+        if damage_dealt >= 4
+            puts "Quel coup de bg ! #{foe.name} s'en est pris plein la gueule ! "
+        elsif damage_dealt == 0
+            puts "#{foe.name} évite le coup ! Quelle classe !"           
+        elsif damage_dealt <= 2
+            puts "C'est à peine si ça lui fait mal..."
+        else
+            puts "Ça fait mal putain !!"    
+        end
+        foe.gets_damage(damage_dealt)
+    end
+
+    def compute_damage
+        return rand(0..6)
+    end
+
+end
